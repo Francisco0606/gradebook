bc750642e110ddf9ac55077b70a3a1d8a0cc7409
Jesse Garza <jesse.garza03@utrgv.edu>
2023-08-17 06:23:40 +0000

    added authorzation and some authincation pluse test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
-  factory :user do
-      sequence(:email) { |n| "person-#{n}@example.com" }
-      password { '123greetings' }
+    factory :user do
+        sequence(:email) { |n| "person-#{n}@example.com" }
+        sequence(:account_id){|n|n}
+        password { '123greetings' }
+  
+        trait :canDel do
+          account_id { 1 }
+        end
+        trait :cantDel do
+          account_id { 0 }
+        end
+    end
   end
-end
